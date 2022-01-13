204b8959a3c520ed4901c9a0663983c67043ce50
Nikhil A <97142198+NIKHILAV-123@users.noreply.github.com>
2022-01-13 09:33:48 +0000

    p1final.c(3)

--- p1final.c
+++ p1final.c
@@ -1,17 +1,21 @@
+//add two numbers
+
 #include<stdio.h>
-void add()
+void add(); //funtion declaration
+int main()
 {
-int num1, num2;
+//int num1=3;
+ add();     //funtion call
+  return 0;
+}
+void add()    //funtion definition
+{
+int num1,num2;
 float sum;
 printf("enter two numbers\n");
 scanf("%d%d", &num1, &num2);
 sum= num1 +num2;
 printf("addition of %d + %d is =%f\n",num2,num1,sum);
-}
-int main()
-{
-  add();
-  return 0;
 }
 
 
