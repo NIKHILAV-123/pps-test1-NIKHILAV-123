dc0c53a128608342892fe3f92eab2af45316c3b2
Nikhil A <97142198+NIKHILAV-123@users.noreply.github.com>
2022-01-25 16:21:33 +0000

    p2original.c

--- p0original.c
+++ p0original.c
@@ -1,6 +1,1 @@
-#include <stdio.h>
-int main()
-{
-  printf("My name is nikhil\n");
-  return 0;
-} 
+ 
--- p1original.c
+++ p1original.c
@@ -1,11 +1,2 @@
-#include<stdio.h>
-int main()
-{
-  int num1 = 4;
-  int num2 = 6;
-  int sum;
-  sum = num1 + num2;
-  printf("value of sum = %d\n", sum);
-  return 0;
-}
 
+}
