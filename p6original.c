f322a52c6c760fe98c3388a4746e38859dd015d2
Nikhil A <97142198+NIKHILAV-123@users.noreply.github.com>
2022-01-27 16:02:02 +0000

    p6original.c

---
+++ p6original.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int input_n()
+{
+  int n;
+  printf("enter number of values to add:\n");
+  scanf("%d",&n);
+  return n;
+}
+int sum_n(int n)
+{
+  int sum=0;
+  for(int i=1;i<=n;i++)
+{
+  sum = sum+i;
+}
+return sum;
+}
+void output(int n, int sum)
+{
+  int i;
+  for(i=1;i<n;i++)
+  {
+    printf("%d+",i);
+
+  }
+  printf("%d=%d",n,sum);
+
+}
+  int main()
+{
+  int a,b;
+  a=input_n();
+  b=sum_n(a);
+  output(a,b);
+}
